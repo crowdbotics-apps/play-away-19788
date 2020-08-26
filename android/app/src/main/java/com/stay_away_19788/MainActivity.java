4006971d60a2566963ebcc3226b2f933de6813f1
Crowdbotics <team@crowdbotics.com>
2020-08-26 02:42:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/stay_away_19788/MainActivity.java
@@ -0,0 +1,15 @@
+package com.stay_away_19788;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "stay_away_19788";
+    }
+}
